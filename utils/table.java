462fd47074aef36b853ac248ec6ab111d638d7b3
Brendan Connolly <brendan.connolly1994@gmail.com>
2019-11-12 21:46:42 +0000

    Added more flesh to table method

--- utils/table.java
+++ utils/table.java
@@ -14,7 +14,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 
 public class table {
-    public final List<String> HEADERS;    
+    public final List<String> HEADERS;
+    public List<List<Object>> DATA;
+    public final List<String> TYPES;
 
     //Constructor when loading table from CSV
     //Inputs:   csvPath - filepath to csv file to read from
@@ -22,6 +24,8 @@ public class table {
     //          head - true if column headers are present in CSV file, false if not
     public table(String csvPath, String schemaPath, boolean head) {
         //Call to XML reader method
+
+        //Initialise data container
 
         //Call to csv loader method
 
@@ -34,13 +38,54 @@ public class table {
     }
 
     //Accessor methods
+    public HashMap<String, Object> getRow(int index) {
 
+    }
 
+    public List<Object> getCol(String colName) {
+
+    }
+
+    public List<String> getHeaders() {
+        return HEADERS;
+    }
+
+    public HashMap<String, String> getSchema() {
+
+    }
 
     //Private class methods
     private List<List<Object>> 
-        loadCSV( String csvPath, List<String> types, boolean head) {
+        loadCSV(String csvPath, boolean head) {
         
+        File file= new File(csvPath);
+        Scanner inputStream;
+
+        try {
+            inputStream = new Scanner(file);
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("File not found at path: " + csvPath)
+            e.printStackTrace();
+        }
+
+        //Make sure to skip the headers if they are present
+        if (head) {
+            inputStream.next();
+        }
+
+        //Loop through the file and split each line on ","
+        //Add the split line to stringVals
+        while(inputStream.hasNext()){
+            String line= inputStream.next();
+            String[] values = line.split(",");
+            //Cast and add line to data
+                stringVals.add(Arrays.asList(values));
+        }
+
+        inputStream.close();
+        
+        return data
     }
 
     private HashMap<String, String> readSchema(String schemaPath) {
